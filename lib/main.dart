c8d42fc49b5074b456591a072cb0e908e194a7c3
Gabriel Cavalcante <gg25gg26@hotmail.com>
2025-02-10 16:40:06 -0300

    configuração inicial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loomi_challange/main_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: const FirebaseOptions(
+      //     apiKey: "AIzaSyAHfsGU0-XC1rWd2PiBe245eOeethkP2tQ",
+      //     appId: "1:787952710825:android:d65eb905a7dcfac58b25b8",
+      //     messagingSenderId: "787952710825",
+      //     projectId: "untold-app-prod"),
+      );
   runApp(
     const MainApp(),
   );
